b1b92712cd21ab9ec762c37be9dd112f10a454eb
kelvinkcc <kelvin.kcc@hotmail.com>
2022-06-27 19:22:53 -0300

    IED-87 subindo ddl e carga de metas

---
+++ BI_Staging/Kelvin/DDL/DDL_OM_METAS_KCCDADOS.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [stg].[OM_METAS](
+	[DATA] [datetime] NULL,
+	[COD_CENTRO_CUSTO] [float] NULL,
+	[CONTA_REDUZIDA] [float] NULL,
+	[META] [float] NULL,
+	ORIGEM VARCHAR(30)
+	)
+	
+	
+	
+	
