c8691501751898c4b1a69e8c93916b80043205e5
YudiSasamori <yudi_sasamori@hotmail.com>
2022-06-26 23:26:45 -0300

    IED-88 e IED-92 Carga tabela OM e OM_Metas

---
+++ BI_Staging/Yudi/DDL/001_DDL_OM_SYM_COMPANY.sql
@@ -0,0 +1,16 @@
+CREATE TABLE [stg].[OM_SYM_COMPANY](
+	[Data] [datetime] NULL,
+	[CONTA_REDUZIDA] [float] NULL,
+	[COD_AREA] [float] NULL,
+	[COD_PACOTE] [float] NULL,
+	[COD_CENTRO_CUSTO] [float] NULL,
+	[DEBITO_CREDITO] [nvarchar](255) NULL,
+	[VALOR_LANCTO] [float] NULL,
+	[DESC_CONTA] [nvarchar](255) NULL,
+	[DESC_AREA] [nvarchar](255) NULL,
+	[DESC_PACOTE] [nvarchar](255) NULL,
+	[DESC_CC] [nvarchar](255) NULL,
+	[GERENTE] [nvarchar](255) NULL,
+	[DIRETOR] [nvarchar](255) NULL,
+	ORIGEM [varchar] (50)
+)
---
+++ BI_Staging/Yudi/DDL/002_DDL_OM_Metas_SYM_COMPANY.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [stg].[OM_Metas_SYM_COMPANY](
+	[DATA] [datetime] NULL,
+	[COD_CENTRO_CUSTO] [float] (50)NULL,
+	[CONTA_REDUZIDA] [float] (50) NULL,
+	[META] [float](50) NULL,
+	ORIGEM [VARCHAR](50)
+)
